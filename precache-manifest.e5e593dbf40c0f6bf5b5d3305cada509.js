425e486783d8dc0d2e3a6f0ad802e53d6f70f1bf
Anton Palgunov <toxblh@gmail.com>
2020-02-18 18:32:15 +0000

    Updates

---
+++ precache-manifest.e5e593dbf40c0f6bf5b5d3305cada509.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "290efcb7b1ff580080aebf9b455472b6",
+    "url": "/react-simple-pdf-viewer/index.html"
+  },
+  {
+    "revision": "d3c58c5758c93380722e",
+    "url": "/react-simple-pdf-viewer/static/css/main.a2b7b0b6.chunk.css"
+  },
+  {
+    "revision": "af746517034140d48853",
+    "url": "/react-simple-pdf-viewer/static/js/2.8a39aae4.chunk.js"
+  },
+  {
+    "revision": "1d59cd014babf6be5ef15a3426f53fc6",
+    "url": "/react-simple-pdf-viewer/static/js/2.8a39aae4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d3c58c5758c93380722e",
+    "url": "/react-simple-pdf-viewer/static/js/main.7960cc7e.chunk.js"
+  },
+  {
+    "revision": "92cc2fc52e53ca6a136b9bd4e4d9a656",
+    "url": "/react-simple-pdf-viewer/static/js/main.7960cc7e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9cc16be204c082f969f6",
+    "url": "/react-simple-pdf-viewer/static/js/runtime-main.981e416b.js"
+  }
+]);
